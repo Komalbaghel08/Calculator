117c5af3e99f0b224bb14b6640ae721fbdf7fcc2
Komalbaghel08 <baghelkomal512@gmail.com>
2025-07-27 16:57:39 +0530

    update

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 let store=""
-let cal=(value)=>{
-  let display = document.querySelector("#screen")
-  store = store+value
+let cal = (value) => {
+  let display = document.querySelector("#screen");
+  const operators = ['+', '-', '*', '/', '%'];
+  let lastChar = store.slice(-1);
 
-  display.innerHTML=store
-}
+  if (operators.includes(lastChar) && operators.includes(value)) {
+    store = store.slice(0, -1) + value;
+  } else {
+    store += value; 
+  }
+
+  display.innerHTML = store;
+};
+
 
 let res=()=>{
   let display = document.querySelector("#screen")
@@ -21,3 +29,5 @@ let delChar = () => {
   store = store.slice(0, -1);
   document.querySelector("#screen").innerHTML = store;
 };
+
+
