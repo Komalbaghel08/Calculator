7533bc3e8a9cadb8497e365a88ac9ce67924c556
Komalbaghel08 <baghelkomal512@gmail.com>
2025-08-26 20:50:13 +0530

    new

--- script.js
+++ script.js
@@ -1,24 +1,26 @@
-let store=""
+let store = "";
 let cal = (value) => {
   let display = document.querySelector("#screen");
-  const operators = ['+', '-', '*', '/', '%'];
+
+  
+  const operators = ["+", "-", "*", "/", "%"];
   let lastChar = store.slice(-1);
 
   if (operators.includes(lastChar) && operators.includes(value)) {
     store = store.slice(0, -1) + value;
   } else {
-    store += value; 
+    store += value;
   }
 
   display.innerHTML = store;
 };
 
 
-let res=()=>{
-  let display = document.querySelector("#screen")
+let res = () => {
+  let display = document.querySelector("#screen");
   store = eval(store).toString();
-  display.innerHTML = store 
-}
+  display.innerHTML = store;
+};
 
 let clr = () => {
   store = "";
@@ -29,5 +31,3 @@ let delChar = () => {
   store = store.slice(0, -1);
   document.querySelector("#screen").innerHTML = store;
 };
-
-
